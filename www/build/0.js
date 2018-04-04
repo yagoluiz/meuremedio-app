f34f2dff8b6d09607801f714bf48b18a521f8945
Yago Luiz <yago.luiz@hotmail.com>
2018-04-04 02:05:46 -0300

    Melhorias de layout android e ios

--- www/build/0.js
+++ www/build/0.js
@@ -1,1 +1,158 @@
-webpackJsonp([0],{244:function(l,n,u){"use strict";function a(l){return r._19(2,[(l()(),r.Z(0,0,null,null,1,"div",[["class","slide-zoom"]],null,null,null,null,null)),r._10(null,0)],null,null)}function o(l){return r._19(2,[(l()(),r.Z(0,0,null,null,3,"div",[["class","swiper-container"]],[[1,"dir",0]],null,null,null,null)),(l()(),r.Z(1,0,null,null,1,"div",[["class","swiper-wrapper"]],null,null,null,null,null)),r._10(null,0),(l()(),r.Z(3,0,null,null,0,"div",[["class","swiper-pagination"]],[[2,"hide",null]],null,null,null,null))],null,function(l,n){var u=n.component;l(n,0,0,u._rtl?"rtl":null);l(n,3,0,!u.pager)})}function i(l){return r._19(0,[(l()(),r.Z(0,0,null,null,5,"button",[["clear",""],["end",""],["icon-end",""],["ion-button",""],["large",""]],null,[[null,"click"]],function(l,n,u){var a=!0;if("click"===n){a=!1!==l.component.iniciarApp()&&a}return a},j.b,j.a)),r.Y(1,1097728,null,0,Z.a,[[8,""],y.a,r.j,r.z],{large:[0,"large"],clear:[1,"clear"]},null),(l()(),r._18(-1,0,["\n        Começar agora\n        "])),(l()(),r.Z(3,0,null,0,1,"ion-icon",[["name","arrow-forward"],["role","img"]],[[2,"hide",null]],null,null,null,null)),r.Y(4,147456,null,0,w.a,[y.a,r.j,r.z],{name:[0,"name"]},null),(l()(),r._18(-1,0,["\n      "]))],function(l,n){l(n,1,0,"","");l(n,4,0,"arrow-forward")},function(l,n){l(n,3,0,r._11(n,4)._hidden)})}function t(l){return r._19(0,[(l()(),r.Z(0,0,null,null,11,"ion-slide",[],null,null,null,a,q)),r.Y(1,180224,null,0,Y.a,[r.j,r.z,A.a],null,null),(l()(),r._18(-1,0,["\n      "])),(l()(),r.Z(3,0,null,0,0,"img",[["class","slide-imagem"]],[[8,"src",4]],null,null,null,null)),(l()(),r._18(-1,0,["\n      "])),(l()(),r.Z(5,0,null,0,0,"h2",[["class","slide-titulo"]],[[8,"innerHTML",1]],null,null,null,null)),(l()(),r._18(-1,0,["\n      "])),(l()(),r.Z(7,0,null,0,0,"p",[],[[8,"innerHTML",1]],null,null,null,null)),(l()(),r._18(-1,0,["\n      "])),(l()(),r.U(16777216,null,0,1,null,i)),r.Y(10,16384,null,0,z.i,[r.I,r.F],{ngIf:[0,"ngIf"]},null),(l()(),r._18(-1,0,["\n    "]))],function(l,n){l(n,10,0,n.component.sair)},function(l,n){l(n,3,0,n.context.$implicit.imagem);l(n,5,0,n.context.$implicit.titulo);l(n,7,0,n.context.$implicit.descricao)})}function e(l){return r._19(0,[(l()(),r.Z(0,0,null,null,10,"ion-header",[["no-shadow",""]],null,null,null,null,null)),r.Y(1,16384,null,0,x.a,[y.a,r.j,r.z,[2,P.a]],null,null),(l()(),r._18(-1,null,["\n  "])),(l()(),r.Z(3,0,null,null,6,"ion-navbar",[["class","toolbar"],["color","primary"]],[[8,"hidden",0],[2,"statusbar-padding",null]],null,null,F.b,F.a)),r.Y(4,49152,null,0,S.a,[T.a,[2,P.a],[2,I.a],y.a,r.j,r.z],{color:[0,"color"]},null),(l()(),r._18(-1,3,["\n    "])),(l()(),r.Z(6,0,null,3,2,"ion-title",[],null,null,null,W.b,W.a)),r.Y(7,49152,null,0,V.a,[y.a,r.j,r.z,[2,B.a],[2,S.a]],null,null),(l()(),r._18(-1,0,["Tutorial"])),(l()(),r._18(-1,3,["\n  "])),(l()(),r._18(-1,null,["\n"])),(l()(),r._18(-1,null,["\n\n"])),(l()(),r.Z(12,0,null,null,9,"ion-content",[["no-bounce",""]],[[2,"statusbar-padding",null],[2,"has-refresher",null]],null,null,E.b,E.a)),r.Y(13,4374528,null,0,L.a,[y.a,M.a,O.a,r.j,r.z,T.a,X.a,r.u,[2,P.a],[2,I.a]],null,null),(l()(),r._18(-1,1,["\n  "])),(l()(),r.Z(15,0,null,1,5,"ion-slides",[["pager",""]],null,[[null,"ionSlideWillChange"]],function(l,n,u){var a=!0;if("ionSlideWillChange"===n){a=!1!==l.component.verificarSlide(u)&&a}return a},o,$)),r.Y(16,1228800,null,0,A.a,[y.a,M.a,r.u,[2,P.a],r.j,r.z],{pager:[0,"pager"],dir:[1,"dir"]},{ionSlideWillChange:"ionSlideWillChange"}),(l()(),r._18(-1,0,["\n    "])),(l()(),r.U(16777216,null,0,1,null,t)),r.Y(19,802816,null,0,z.h,[r.I,r.F,r.p],{ngForOf:[0,"ngForOf"]},null),(l()(),r._18(-1,0,["\n  "])),(l()(),r._18(-1,1,["\n"]))],function(l,n){var u=n.component;l(n,4,0,"primary");l(n,16,0,"",r._2(1,"",u.dir,""));l(n,19,0,u.slides)},function(l,n){l(n,3,0,r._11(n,4)._hidden,r._11(n,4)._sbPadding);l(n,12,0,r._11(n,13).statusbarPadding,r._11(n,13)._hasRefresher)})}Object.defineProperty(n,"__esModule",{value:!0});var r=u(0),s=(u(5),u(114),u(200)),c=u(245),d=function(){function l(l,n,u){this.navCtrl=l,this.menuCtrl=n,this.platform=u,this.sair=!1,this.dir="ltr",c.a.sendPageViewForPage("/tutorial"),this.dir=u.dir(),this.slides=[{titulo:"Bem-vindo ao meu remédio :)",descricao:"O <strong>meu remédio</strong> é um app para consulta de preços de remédios. Aqui você encontra informações de preços máximos que podem ser oferecidos para o <strong>consumidor</strong>, preços máximos que as <strong>farmácias</strong> podem adquirir o remédio das fábricas e detalhes sobre o remédio.",imagem:"assets/img/logo-original.png"},{titulo:"Como usar o meu rémedio?",descricao:"Como no exemplo acima, pesquise o <strong>nome</strong> do remédio desejado para consultar as informações. A <strong>cor</strong> ao lado indicará a tarja do remédio. Bem fácil, não é mesmo?",imagem:"assets/img/pagina-pesquisa.png"}]}return l.prototype.iniciarApp=function(){c.a.sendEvent("click","Tutorial:Iniciar:App"),this.navCtrl.setRoot(s.b,{},{animate:!0,direction:"forward"})},l.prototype.verificarSlide=function(l){this.sair=l.isEnd()},l.prototype.ionViewDidEnter=function(){this.menuCtrl.enable(!1)},l.prototype.ionViewWillLeave=function(){this.menuCtrl.enable(!0)},l}(),g=function(){return function(){}}(),p=u(189),m=u(190),_=u(191),b=u(192),f=u(193),v=u(194),h=u(195),k=u(196),C=u(197),j=u(43),Z=u(20),y=u(1),w=u(42),Y=u(121),A=u(74),q=r.X({encapsulation:2,styles:[],data:{}}),z=u(11),x=u(73),P=u(4),F=u(246),S=u(33),T=u(8),I=u(19),W=u(198),V=u(53),B=u(34),E=u(199),L=u(21),M=u(3),O=u(9),X=u(24),$=r.X({encapsulation:2,styles:[],data:{}}),H=u(22),R=r.X({encapsulation:2,styles:[],data:{}}),U=r.V("page-tutorial",d,function(l){return r._19(0,[(l()(),r.Z(0,0,null,null,1,"page-tutorial",[],null,null,null,e,R)),r.Y(1,49152,null,0,d,[I.a,H.a,M.a],null,null)],null,null)},{},{},[]),D=u(18),J=u(115),N=u(37);u.d(n,"TutorialPageModuleNgFactory",function(){return G});var G=r.W(g,[],function(l){return r._7([r._8(512,r.i,r.S,[[8,[p.a,m.a,_.a,b.a,f.a,v.a,h.a,k.a,C.a,U]],[3,r.i],r.s]),r._8(4608,z.k,z.j,[r.r,[2,z.s]]),r._8(4608,D.k,D.k,[]),r._8(4608,D.c,D.c,[]),r._8(512,z.b,z.b,[]),r._8(512,D.j,D.j,[]),r._8(512,D.d,D.d,[]),r._8(512,D.i,D.i,[]),r._8(512,J.a,J.a,[]),r._8(512,J.b,J.b,[]),r._8(512,g,g,[]),r._8(256,N.a,d,[])])})},245:function(l,n,u){"use strict";u.d(n,"a",function(){return a});var a=function(){function l(){}return l.googleAnalytics=function(){for(var l=[],n=0;n<arguments.length;n++)l[n]=arguments[n];(u=window).ga.apply(u,l);var u},l.sendPageViewForPage=function(n){l.googleAnalytics("set","page",n),l.googleAnalytics("send","pageview")},l.sendEvent=function(n,u,a){void 0===a&&(a=""),l.googleAnalytics("send",{hitType:"event",eventCategory:n,eventAction:u,eventLabel:a})},l}()},246:function(l,n,u){"use strict";function a(l){return o._19(0,[(l()(),o.Z(0,0,null,null,1,"div",[["class","toolbar-background"]],null,null,null,null,null)),o.Y(1,278528,null,0,i.g,[o.p,o.q,o.j,o.A],{klass:[0,"klass"],ngClass:[1,"ngClass"]},null),(l()(),o.Z(2,0,null,null,8,"button",[["class","back-button"],["ion-button","bar-button"]],[[8,"hidden",0]],[[null,"click"]],function(l,n,u){var a=!0;if("click"===n){a=!1!==l.component.backButtonClick(u)&&a}return a},t.b,t.a)),o.Y(3,278528,null,0,i.g,[o.p,o.q,o.j,o.A],{klass:[0,"klass"],ngClass:[1,"ngClass"]},null),o.Y(4,1097728,null,0,e.a,[[8,"bar-button"],r.a,o.j,o.z],null,null),(l()(),o.Z(5,0,null,0,2,"ion-icon",[["class","back-button-icon"],["role","img"]],[[2,"hide",null]],null,null,null,null)),o.Y(6,278528,null,0,i.g,[o.p,o.q,o.j,o.A],{klass:[0,"klass"],ngClass:[1,"ngClass"]},null),o.Y(7,147456,null,0,s.a,[r.a,o.j,o.z],{name:[0,"name"]},null),(l()(),o.Z(8,0,null,0,2,"span",[["class","back-button-text"]],null,null,null,null,null)),o.Y(9,278528,null,0,i.g,[o.p,o.q,o.j,o.A],{klass:[0,"klass"],ngClass:[1,"ngClass"]},null),(l()(),o._18(10,null,["",""])),o._10(null,0),o._10(null,1),o._10(null,2),(l()(),o.Z(14,0,null,null,2,"div",[["class","toolbar-content"]],null,null,null,null,null)),o.Y(15,278528,null,0,i.g,[o.p,o.q,o.j,o.A],{klass:[0,"klass"],ngClass:[1,"ngClass"]},null),o._10(null,3)],function(l,n){var u=n.component;l(n,1,0,"toolbar-background","toolbar-background-"+u._mode);l(n,3,0,"back-button","back-button-"+u._mode);l(n,6,0,"back-button-icon","back-button-icon-"+u._mode);l(n,7,0,u._bbIcon);l(n,9,0,"back-button-text","back-button-text-"+u._mode);l(n,15,0,"toolbar-content","toolbar-content-"+u._mode)},function(l,n){var u=n.component;l(n,2,0,u._hideBb);l(n,5,0,o._11(n,7)._hidden);l(n,10,0,u._backText)})}u.d(n,"a",function(){return c}),n.b=a;var o=u(0),i=u(11),t=u(43),e=u(20),r=u(1),s=u(42),c=(u(4),u(19),o.X({encapsulation:2,styles:[],data:{}}))}});
+webpackJsonp([0],{
+
+/***/ 283:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TutorialPageModule", function() { return TutorialPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(98);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__tutorial__ = __webpack_require__(288);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var TutorialPageModule = /** @class */ (function () {
+    function TutorialPageModule() {
+    }
+    TutorialPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__tutorial__["a" /* TutorialPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__tutorial__["a" /* TutorialPage */])
+            ],
+            exports: [
+                __WEBPACK_IMPORTED_MODULE_2__tutorial__["a" /* TutorialPage */]
+            ]
+        })
+    ], TutorialPageModule);
+    return TutorialPageModule;
+}());
+
+//# sourceMappingURL=tutorial.module.js.map
+
+/***/ }),
+
+/***/ 284:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return GoogleAnalytics; });
+var GoogleAnalytics = /** @class */ (function () {
+    function GoogleAnalytics() {
+    }
+    GoogleAnalytics.googleAnalytics = function () {
+        var fields = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            fields[_i] = arguments[_i];
+        }
+        (_a = window).ga.apply(_a, fields);
+        var _a;
+    };
+    // https://developers.google.com/analytics/devguides/collection/analyticsjs/single-page-applications?hl=pt-br
+    GoogleAnalytics.sendPageViewForPage = function (nomePage) {
+        GoogleAnalytics.googleAnalytics('set', 'page', nomePage);
+        GoogleAnalytics.googleAnalytics('send', 'pageview');
+    };
+    // https://developers.google.com/analytics/devguides/collection/analyticsjs/events?hl=pt-br
+    GoogleAnalytics.sendEvent = function (eventCategory, eventAction, eventLabel) {
+        if (eventLabel === void 0) { eventLabel = ''; }
+        GoogleAnalytics.googleAnalytics('send', {
+            hitType: 'event',
+            eventCategory: eventCategory,
+            eventAction: eventAction,
+            eventLabel: eventLabel
+        });
+    };
+    return GoogleAnalytics;
+}());
+
+//# sourceMappingURL=google-analytics.js.map
+
+/***/ }),
+
+/***/ 288:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TutorialPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(98);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__pages__ = __webpack_require__(199);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_google_analytics__ = __webpack_require__(284);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+var TutorialPage = /** @class */ (function () {
+    function TutorialPage(navCtrl, menuCtrl, platform) {
+        this.navCtrl = navCtrl;
+        this.menuCtrl = menuCtrl;
+        this.platform = platform;
+        this.sair = false;
+        this.dir = 'ltr';
+        __WEBPACK_IMPORTED_MODULE_3__app_google_analytics__["a" /* GoogleAnalytics */].sendPageViewForPage('/tutorial');
+        this.dir = platform.dir();
+        this.slides = [
+            {
+                titulo: 'Bem-vindo ao meu remédio :)',
+                descricao: 'O <strong>meu remédio</strong> é um app para consulta de preços de remédios. Aqui você encontra informações de preços máximos que podem ser oferecidos para o <strong>consumidor</strong>, preços máximos que as <strong>farmácias</strong> podem adquirir o remédio das fábricas e detalhes sobre o remédio.',
+                imagem: 'assets/img/logo-original.png',
+            },
+            {
+                titulo: 'Como usar o meu rémedio?',
+                descricao: 'Como no exemplo acima, pesquise o <strong>nome</strong> do remédio desejado para consultar as informações. A <strong>cor</strong> ao lado indicará a tarja do remédio. Bem fácil, não é mesmo?',
+                imagem: 'assets/img/pagina-pesquisa.png',
+            }
+        ];
+    }
+    TutorialPage.prototype.iniciarApp = function () {
+        __WEBPACK_IMPORTED_MODULE_3__app_google_analytics__["a" /* GoogleAnalytics */].sendEvent('click', "Tutorial:Iniciar:App");
+        this.navCtrl.setRoot(__WEBPACK_IMPORTED_MODULE_2__pages__["b" /* MainPage */], {}, {
+            animate: true,
+            direction: 'forward'
+        });
+    };
+    TutorialPage.prototype.verificarSlide = function (slider) {
+        this.sair = slider.isEnd();
+    };
+    TutorialPage.prototype.ionViewDidEnter = function () {
+        this.menuCtrl.enable(false);
+    };
+    TutorialPage.prototype.ionViewWillLeave = function () {
+        this.menuCtrl.enable(true);
+    };
+    TutorialPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-tutorial',template:/*ion-inline-start:"C:\Dev\Ionic\UnB\meuremedio-app\src\pages\tutorial\tutorial.html"*/'<ion-header no-shadow>\n  <ion-navbar color="primary">\n    <ion-title>Tutorial</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content no-bounce>\n  <ion-slides dir="{{dir}}" (ionSlideWillChange)="verificarSlide($event)" pager>\n    <ion-slide *ngFor="let slide of slides">\n      <img [src]="slide.imagem" class="slide-imagem" />\n      <h2 class="slide-titulo" [innerHTML]="slide.titulo"></h2>\n      <p [innerHTML]="slide.descricao"></p>\n      <button ion-button icon-end large clear (click)="iniciarApp()" end *ngIf="sair">\n        Começar agora\n        <ion-icon name="arrow-forward"></ion-icon>\n      </button>\n    </ion-slide>\n  </ion-slides>\n</ion-content>'/*ion-inline-end:"C:\Dev\Ionic\UnB\meuremedio-app\src\pages\tutorial\tutorial.html"*/
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavController */],
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* MenuController */],
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* Platform */]])
+    ], TutorialPage);
+    return TutorialPage;
+}());
+
+//# sourceMappingURL=tutorial.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
